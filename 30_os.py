e386ad5e4b23fd796238c9d2a58459adfae747e2
nnnick2kim <huxin_star@126.com>
2018-03-15 14:57:30 +0800

    add 30_os.py

---
+++ 30_os.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf8 -*-
+
+import platform
+
+os = platform.system()
+bit = platform.architecture()
+ver = platform.version()
+result = os + ' ' + bit[0]
+print result
